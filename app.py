69cbda31927b4c51e76c7fe68a8339757fc2c7da
James <james.labyer@gmail.com>
2024-10-16 14:11:08 -0500

    3 cross-section graphs, 1 set of controls

--- app.py
+++ app.py
@@ -3,43 +3,166 @@ from dash import Dash, dcc, html, callback, Input, Output
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 import numpy as np
+import math
 
 app = Dash(external_stylesheets=[dbc.themes.LUMEN])
 
-df = pd.read_csv("assets/rectangles_output.csv")
+LAYERS = ["All", "Layer 1", "Layer 2", "Layer 3", "Layer 4"]
+X_AXIS = [221, 271, 321, 371, 421, 471, 521, 571, 621, 671]
+Y_AXIS = [248, 301, 354, 407, 460, 513]
+Z_AXIS = [0, 35, 70, 105, 140]
+
+C_SCHEMES = [
+    "bluered",
+    "deep",
+    "delta",
+    "greys",
+    "haline",
+    "ice",
+    "inferno",
+    "jet",
+    "magma",
+    "plasma",
+    "spectral",
+    "thermal",
+    "viridis",
+    "ylgnbu",
+    "ylorrd",
+]
+
+cubes_df1 = pd.read_csv("assets/rectangles_output.csv")
+points_df = pd.read_csv("assets/HuBMAP_ili_data10-11-24.csv")
+protein_df = pd.read_csv("assets/protein_labels.csv")
+proteins = protein_df.columns.tolist()
 
 
-X = df.loc[:, "X Center"]
-Y = df.loc[:, "Y Center"]
-Z = df.loc[:, "Z Center"]
-values = df.loc[:, "CYB5A"]
+def select_layer(zlayer, df):
+    if zlayer == "All":
+        return df
+    elif zlayer == "Layer 1":
+        return df[df["Z Center"] < 35]
+    elif zlayer == "Layer 2":
+        return df[(df["Z Center"] >= 35) & (df["Z Center"] < 70)]
+    elif zlayer == "Layer 3":
+        return df[(df["Z Center"] >= 70) & (df["Z Center"] < 105)]
+    elif zlayer == "Layer 4":
+        return df[df["Z Center"] >= 105]
 
-fig1 = go.Figure(
-    data=go.Volume(
-        x=X,
-        y=Y,
-        z=Z,
-        value=values,
-        isomin=-10,
-        isomax=10,
-        opacity=0.5,
-        surface_count=180,
-        slices_z=dict(show=True, locations=[0.4]),
-        caps=dict(x_show=True, y_show=True, z_show=True, x_fill=1),
-        colorscale="Jet",
+
+def get_colors(df, protein):
+    colors = [
+        df[df["Y Center"] < 301].loc[:, protein].to_list(),
+        df[(df["Y Center"] >= 301) & (df["Y Center"] < 354)].loc[:, protein].to_list(),
+        df[(df["Y Center"] >= 354) & (df["Y Center"] < 407)].loc[:, protein].to_list(),
+        df[(df["Y Center"] >= 407) & (df["Y Center"] < 460)].loc[:, protein].to_list(),
+        df[df["Y Center"] >= 460].loc[:, protein].to_list(),
+    ]
+    return colors
+
+
+def make_fig1(opacity=0.4, caps=True, colorscheme="Jet", protein="CYB5A", layer="All"):
+    df3 = select_layer(layer, cubes_df1)
+
+    X = df3.loc[:, "X Center"]
+    Y = df3.loc[:, "Y Center"]
+    Z = df3.loc[:, "Z Center"]
+    values = df3.loc[:, protein]
+
+    fig1 = go.Figure(
+        data=go.Volume(
+            x=X,
+            y=Y,
+            z=Z,
+            value=values,
+            isomin=math.floor(protein_df.loc[0, protein]),
+            isomax=math.ceil(protein_df.loc[1, protein]),
+            opacity=opacity,
+            surface_count=180,
+            slices_z=dict(show=True, locations=[0.4]),
+            caps=dict(x_show=caps, y_show=caps, z_show=caps, x_fill=1),
+            colorscale=colorscheme,
+        )
     )
-)
 
-fig1.update_layout(
-    scene=dict(
-        xaxis=dict(tickvals=[221, 271, 321, 421, 471, 521, 571, 621, 671]),
-        yaxis=dict(tickvals=[248, 301, 354, 407, 460, 513]),
-        zaxis=dict(tickvals=[0, 35, 70, 105, 140]),
+    fig1.update_layout(
+        scene=dict(
+            xaxis=dict(tickvals=X_AXIS),
+            yaxis=dict(tickvals=Y_AXIS),
+            zaxis=dict(tickvals=Z_AXIS),
+        )
     )
-)
+    return fig1
 
-df2 = pd.read_csv("assets/protein_labels.csv")
-proteins = df2.columns.tolist()
+
+def make_fig2(opacity=0.1, colorscheme="Jet", protein="CYB5A", layer="All"):
+    df4 = select_layer(layer, points_df)
+    X = df4.loc[:, "X Center"]
+    Y = df4.loc[:, "Y Center"]
+    Z = df4.loc[:, "Z Center"]
+    values = df4.loc[:, protein]
+
+    fig2 = go.Figure(
+        data=go.Volume(
+            x=X,
+            y=Y,
+            z=Z,
+            value=values,
+            isomin=math.floor(protein_df.loc[0, protein]),
+            isomax=math.ceil(protein_df.loc[1, protein]),
+            opacity=opacity,
+            colorscale=colorscheme,
+            surface_count=21,
+        )
+    )
+    return fig2
+
+
+def make_fig3(cscheme="Jet", protein="CYB5A"):
+    x = X_AXIS[:9]
+    y = Y_AXIS[:5]
+    z1 = [[35 for i in x] for j in y]
+    z2 = [[70 for i in x] for j in y]
+    z3 = [[105 for i in x] for j in y]
+    z4 = [[140 for i in x] for j in y]
+
+    color_sets = []
+
+    for k in range(4):
+        this_layer = select_layer(LAYERS[k], points_df)
+        color_sets.append(get_colors(this_layer, protein))
+
+    fig = go.Figure(
+        data=[
+            go.Surface(x=x, y=y, z=z1, colorscale=cscheme, surfacecolor=color_sets[0]),
+            go.Surface(
+                x=x,
+                y=y,
+                z=z2,
+                showscale=False,
+                colorscale=cscheme,
+                surfacecolor=color_sets[1],
+            ),
+            go.Surface(
+                x=x,
+                y=y,
+                z=z3,
+                showscale=False,
+                colorscale=cscheme,
+                surfacecolor=color_sets[2],
+            ),
+            go.Surface(
+                x=x,
+                y=y,
+                z=z4,
+                showscale=False,
+                colorscale=cscheme,
+                surfacecolor=color_sets[3],
+            ),
+        ]
+    )
+
+    return fig
+
 
 app.layout = html.Div(
     children=[
@@ -93,12 +216,12 @@ app.layout = html.Div(
                                 ),
                                 html.P("Select a tissue layer to view"),
                                 dcc.Dropdown(
-                                    ["All", "Layer 1", "Layer 2", "Layer 3", "Layer 4"],
+                                    LAYERS,
                                     "All",
                                     id="layersdd",
                                 ),
                                 dcc.Graph(
-                                    figure=fig1,
+                                    figure=make_fig1(),
                                     className="dcc-graph",
                                     id="cross-section-graph",
                                 ),
@@ -124,17 +247,7 @@ app.layout = html.Div(
                                                             "Choose a color scheme:"
                                                         ),
                                                         dcc.Dropdown(
-                                                            [
-                                                                "YlGnBu",
-                                                                "Bluered",
-                                                                "Blues",
-                                                                "Greens",
-                                                                "Greys",
-                                                                "Jet",
-                                                                "Reds",
-                                                                "Viridis",
-                                                                "YlOrRd",
-                                                            ],
+                                                            C_SCHEMES,
                                                             "Jet",
                                                             id="colorschemedd",
                                                         ),
@@ -160,6 +273,24 @@ app.layout = html.Div(
                                         ),
                                     ],
                                     id="cross-section-controls",
+                                ),
+                                html.Div(
+                                    children=[
+                                        dcc.Graph(
+                                            figure=make_fig2(),
+                                            className="dcc-graph",
+                                            id="cross-section-graph2",
+                                        ),
+                                    ]
+                                ),
+                                html.Div(
+                                    children=[
+                                        dcc.Graph(
+                                            figure=make_fig3(),
+                                            className="dcc-graph",
+                                            id="cross-section-graph3",
+                                        ),
+                                    ]
                                 ),
                             ],
                         ),
@@ -223,21 +354,10 @@ app.layout = html.Div(
 )
 
 
-def select_layer(zlayer):
-    if zlayer == "All":
-        return df
-    elif zlayer == "Layer 1":
-        return df[df["Z Center"] < 35]
-    elif zlayer == "Layer 2":
-        return df[(df["Z Center"] >= 35) & (df["Z Center"] < 70)]
-    elif zlayer == "Layer 3":
-        return df[(df["Z Center"] >= 70) & (df["Z Center"] < 105)]
-    elif zlayer == "Layer 4":
-        return df[df["Z Center"] >= 105]
-
-
 @callback(
     Output("cross-section-graph", "figure"),
+    Output("cross-section-graph2", "figure"),
+    Output("cross-section-graph3", "figure"),
     Input("opacityslider", "value"),
     Input("capswitch", "value"),
     Input("colorschemedd", "value"),
@@ -245,31 +365,10 @@ def select_layer(zlayer):
     Input("layersdd", "value"),
 )
 def update_output(opacityslider, capswitch, colorschemedd, proteinsdd, layersdd):
-    df1 = select_layer(layersdd)
-    # print(df[(df["Z Center"] >= 35) & (df["Z Center"] < 70)].loc[:, "X Center"])
-    fig1 = go.Figure(
-        data=go.Volume(
-            x=df1.loc[:, "X Center"],
-            y=df1.loc[:, "Y Center"],
-            z=df1.loc[:, "Z Center"],
-            value=df1.loc[:, proteinsdd],
-            isomin=-10,
-            isomax=10,
-            opacity=opacityslider,
-            surface_count=180,
-            slices_z=dict(show=True, locations=[0.4]),
-            caps=dict(x_show=capswitch, y_show=capswitch, z_show=capswitch, x_fill=1),
-            colorscale=colorschemedd,
-        )
-    )
-    fig1.update_layout(
-        scene=dict(
-            xaxis=dict(tickvals=[221, 271, 321, 371, 421, 471, 521, 571, 621, 671]),
-            yaxis=dict(tickvals=[248, 301, 354, 407, 460, 513]),
-            zaxis=dict(tickvals=[0, 35, 70, 105, 140]),
-        )
-    )
-    return fig1
+    fig1 = make_fig1(opacityslider, capswitch, colorschemedd, proteinsdd, layersdd)
+    fig2 = make_fig2(opacityslider, colorschemedd, proteinsdd, layersdd)
+    fig3 = make_fig3(colorschemedd, proteinsdd)
+    return fig1, fig2, fig3
 
 
 @callback(
