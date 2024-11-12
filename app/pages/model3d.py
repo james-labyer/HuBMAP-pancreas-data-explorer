0d4361bbbeb160b3e3da5dcdd6107533192648ac
James <james.labyer@gmail.com>
2024-11-12 16:09:19 -0600

    Finish 3d model for pancreas 1 and add tests

---
+++ app/pages/model3d.py
@@ -0,0 +1,190 @@
+import pandas as pd
+from dash import html, dcc, register_page, Output, Input, callback
+import dash_bootstrap_components as dbc
+import numpy as np
+from pywavefront import Wavefront
+import plotly.graph_objects as go
+
+register_page(__name__, path="/3d", title="3D Pancreas Model")
+
+blocks = pd.read_csv("assets/block-data.csv")
+traces = pd.read_csv("assets/obj/obj-files.csv")
+
+
+def read_obj(file):
+    pancreas = Wavefront(file, collect_faces=True)
+    matrix_vertices = np.array(pancreas.vertices)
+    faces = np.array(pancreas.mesh_list[0].faces)
+    return matrix_vertices, faces
+
+
+def make_mesh_settings(
+    vertices,
+    faces,
+    color="cyan",
+    opacity=1,
+):
+
+    x, y, z = vertices.T
+    I, J, K = faces.T
+
+    mesh = {
+        "type": "mesh3d",
+        "x": y,
+        "y": z,
+        "z": x,
+        # "x": x,
+        # "y": y,
+        # "z": z,
+        "color": color,
+        "flatshading": False,
+        "opacity": opacity,
+        "i": I,
+        "j": J,
+        "k": K,
+        "name": "Pancreas",
+        "showscale": None,
+        "lighting": {
+            "ambient": 0.18,
+            "diffuse": 1,
+            "fresnel": 0.1,
+            "specular": 1,
+            "roughness": 0.1,
+            "facenormalsepsilon": 1e-6,
+            "vertexnormalsepsilon": 1e-12,
+        },
+        "lightposition": {"x": 100, "y": 200, "z": 0},
+    }
+
+    return [mesh]
+
+
+def make_mesh_data(name, file, color=None, opacity=1):
+    vertices, faces = read_obj(file)
+    data = make_mesh_settings(vertices, faces, color=color, opacity=opacity)
+    data[0]["name"] = name
+    return data
+
+
+def make_mesh_fig(pancreas=1):
+
+    pancreas_traces = traces.loc[traces["pancreas"] == pancreas]
+    for i in range(pancreas_traces.shape[0]):
+        if i == 0:
+            data1 = make_mesh_data(
+                pancreas_traces.at[i, "name"],
+                pancreas_traces.at[i, "file"],
+                pancreas_traces.at[i, "color"],
+                pancreas_traces.at[i, "opacity"],
+            )
+            fig = go.Figure(data1)
+        else:
+            data = make_mesh_data(
+                pancreas_traces.at[i, "name"],
+                pancreas_traces.at[i, "file"],
+                pancreas_traces.at[i, "color"],
+                pancreas_traces.at[i, "opacity"],
+            )
+            fig.add_trace(go.Mesh3d(data[0]))
+    fig.update_layout(
+        height=500,
+        scene=dict(
+            xaxis=dict(visible=False),
+            yaxis=dict(visible=False),
+            zaxis=dict(visible=False),
+        ),
+        margin=dict(l=20, r=20, t=20, b=20),
+    )
+    return fig
+
+
+def make_graph_layout(pancreas=1):
+    container = dbc.Container(
+        [
+            dbc.Row(
+                dbc.Col(
+                    html.H2(f"3D Model of Pancreas {pancreas}"),
+                )
+            ),
+            dbc.Row(
+                [
+                    dbc.Col(
+                        [
+                            dcc.Graph(
+                                id="pancreas-graph",
+                                figure=make_mesh_fig(pancreas),
+                                config={"scrollZoom": False},
+                                className="centered-graph",
+                            )
+                        ],
+                        width=9,
+                    ),
+                    dbc.Col(
+                        dbc.Card(
+                            id="click-data",
+                            color="light",
+                            class_name="block-card",
+                        ),
+                        width=3,
+                    ),
+                ],
+                className="g-2",
+            ),
+        ],
+        fluid=True,
+        class_name="model-page",
+    )
+    return container
+
+
+layout = html.Div([make_graph_layout(1)])
+
+
+@callback(Output("click-data", "children"), [Input("pancreas-graph", "clickData")])
+def display_click_data(click_data):
+    if click_data and click_data["points"][0]["curveNumber"] > 3:
+        row = blocks.loc[
+            blocks["Block ID"]
+            == traces.loc[click_data["points"][0]["curveNumber"], "name"]
+        ]
+        item_data = [
+            {"label": "Block ", "value": row.iloc[0]["Block ID"]},
+            {"label": "Anatomical region: ", "value": row.iloc[0]["Anatomical region"]},
+            {
+                "label": "View optical clearing data",
+                "value": row.iloc[0]["Optical clearing"],
+            },
+            {"label": "View GeoMX data", "value": row.iloc[0]["GeoMX"]},
+            {"label": "View proteomics data", "value": row.iloc[0]["Proteomics"]},
+        ]
+        card_content = []
+        card_body_content = []
+        for i in range(5):
+            label_str = str(item_data[i]["label"])
+            value_str = str(item_data[i]["value"])
+            if i == 0:
+                card_content.append(
+                    dbc.CardHeader(label_str + value_str, class_name="card-title")
+                )
+            elif i == 1:
+                child = html.P(label_str + value_str)
+                card_body_content.append(child)
+            elif item_data[i]["value"] == " ":
+                continue
+            else:
+                value_str = item_data[i]["value"]
+                child = html.P([dcc.Link(label_str, href=f"{value_str}")])
+                card_body_content.append(child)
+        card_content.append(dbc.CardBody(card_body_content))
+        return card_content
+    else:
+        content = "Click on a block to view available datasets"
+        card_content = [
+            dbc.CardHeader("Block Data"),
+            dbc.CardBody(
+                [
+                    html.P(content),
+                ],
+            ),
+        ]
+        return card_content
---
+++ app/tests/test_3d.py
@@ -0,0 +1,140 @@
+import pytest
+import sys
+from pathlib import Path
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+import json
+import plotly
+
+sys.path.append(str(Path(__file__).parent.parent.parent))
+import app.app
+from app.pages.model3d import (
+    display_click_data,
+    make_mesh_settings,
+    read_obj,
+    make_mesh_data,
+    make_mesh_fig,
+)
+
+
+def test_read_obj():
+    vertices, faces = read_obj("./assets/obj/P1_Pancreas_Head_4A.obj")
+    assert vertices.shape == (8, 3)
+    assert faces.shape == (12, 3)
+
+    vertices, faces = read_obj("./assets/obj/head_invert.obj")
+    assert vertices.shape == (269694, 3)
+    assert faces.shape == (535314, 3)
+
+    with pytest.raises(IndexError):
+        vertices, faces = read_obj("./assets/obj/obj-files.csv")
+
+
+def test_make_mesh_settings():
+    vertices, faces = read_obj("./assets/obj/P1_Pancreas_Head_4A.obj")
+    settings1 = make_mesh_settings(
+        vertices,
+        faces,
+        color="cyan",
+        opacity=1,
+    )
+
+    assert settings1[0]["color"] == "cyan"
+    assert settings1[0]["opacity"] == 1
+
+    vertices, faces = read_obj("./assets/obj/P1_Pancreas_Head_4A.obj")
+    settings2 = make_mesh_settings(
+        vertices,
+        faces,
+        color="#ED780B",
+        opacity=0.5,
+    )
+
+    assert settings2[0]["color"] == "#ED780B"
+    assert settings2[0]["opacity"] == 0.5
+
+
+def test_make_mesh_data():
+    test1 = make_mesh_data("Head", "./assets/obj/head_invert.obj")
+    assert test1[0]["name"] == "Head"
+
+    test2 = make_mesh_data("Tail", "./assets/obj/tail_invert.obj")
+    assert test2[0]["name"] == "Tail"
+
+
+def test_make_mesh_fig():
+    fig1 = make_mesh_fig(1)
+    assert len(fig1["data"]) == 12
+    assert fig1["layout"]["height"] == 500
+
+
+def test_display_click_data():
+    click1 = {
+        "points": [
+            {
+                "x": -392,
+                "y": 1749.50244140625,
+                "z": 746,
+                "curveNumber": 3,
+                "pointNumber": 52662,
+                "i": 28916,
+                "j": 28769,
+                "k": 28768,
+                "bbox": {
+                    "x0": 539.0667184592023,
+                    "x1": 539.0667184592023,
+                    "y0": 401.4491349814796,
+                    "y1": 401.4491349814796,
+                },
+            }
+        ]
+    }
+    layout1 = json.loads(
+        json.dumps(display_click_data(click1), cls=plotly.utils.PlotlyJSONEncoder)
+    )
+    testlayout1 = [
+        dbc.CardHeader("Block Data"),
+        dbc.CardBody(
+            [
+                html.P("Click on a block to view available datasets"),
+            ],
+        ),
+    ]
+    test1 = json.loads(json.dumps(testlayout1, cls=plotly.utils.PlotlyJSONEncoder))
+    assert layout1 == test1
+
+    click2 = {
+        "points": [
+            {
+                "x": -392,
+                "y": 1749.50244140625,
+                "z": 746,
+                "curveNumber": 4,
+                "pointNumber": 52662,
+                "i": 28916,
+                "j": 28769,
+                "k": 28768,
+                "bbox": {
+                    "x0": 539.0667184592023,
+                    "x1": 539.0667184592023,
+                    "y0": 401.4491349814796,
+                    "y1": 401.4491349814796,
+                },
+            }
+        ]
+    }
+    layout2 = json.loads(
+        json.dumps(display_click_data(click2), cls=plotly.utils.PlotlyJSONEncoder)
+    )
+    testlayout2 = [
+        dbc.CardHeader("Block P1-4A", class_name="card-title"),
+        dbc.CardBody(
+            [
+                html.P("Anatomical region: Pancreatic head"),
+                html.P([dcc.Link("View optical clearing data", href="/test")]),
+                html.P([dcc.Link("View GeoMX data", href="/test")]),
+            ]
+        ),
+    ]
+    test2 = json.loads(json.dumps(testlayout2, cls=plotly.utils.PlotlyJSONEncoder))
+    assert layout2 == test2
